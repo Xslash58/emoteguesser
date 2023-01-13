0e1da006ff28c8276b1b627898ace5e7da8a7184
Xslash <kapimak28@gmail.com>
2023-01-13 23:10:24 +0100

    Remove dev version warning

--- Assets/__Scripts/GLOBAL/GLOBAL.cs
+++ Assets/__Scripts/GLOBAL/GLOBAL.cs
@@ -6,15 +6,4 @@ public class GLOBAL : MonoBehaviour
     {
         DontDestroyOnLoad(gameObject);
     }
-    private void Start()
-    {
-        if (Application.version.Contains("DEV") || Debug.isDebugBuild)
-        {
-            INFOBOX.instance.Request(new INFOBOX.INFOBOXMessage()
-            {
-                title = "<color=#FF0000>WARNING",
-                description = "You're using developer version of <b>Emote Guesser</b>. Official game is <b>ONLY</b> downloadable from <color=#FF0000><b>xslash.itch.io/emoteguesser</b></color>. Make sure that you got this version from game developer."
-            });
-        }
-    }
 }
