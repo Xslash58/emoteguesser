ec30a69bcde1bcf346d4629856e58734fc3e0e76
Xslash <kapimak28@gmail.com>
2023-01-17 23:52:21 +0100

    Import Unity-Twitch-Chat by lexone

---
+++ Assets/Unity-Twitch-Chat/Chatter.cs
@@ -0,0 +1,58 @@
+
+using UnityEngine;
+
+namespace Lexone.UnityTwitchChat
+{
+    [System.Serializable]
+    public class Chatter
+    {
+        public Chatter(string login, string channel, string message, IRCTags tags)
+        {
+            this.login = login;
+            this.channel = channel;
+            this.message = message;
+            this.tags = tags;
+        }
+
+        public string login, channel, message;
+        public IRCTags tags = null;
+
+        /// <summary>
+        /// <para>Returns the RGBA color of the chatter's name (tags.colorHex)</para>
+        /// <para>Note that the color will be white if the user has not set their name color</para>
+        /// </summary>
+        public Color GetNameColor()
+        {
+            if (
+                tags.colorHex.Length > 0 &&
+                ColorUtility.TryParseHtmlString(tags.colorHex, out Color color)
+            )
+                return color;
+            else
+                // User has not set a name color, or parsing failed somehow -> return white
+                // TODO: Return a random color instead (Native Twitch chat does this, although it's not fully random)
+                return new Color(1, 1, 1, 1);
+        }
+
+        /// <summary>
+        /// <para>Returns true if displayName is "font-safe" 
+        /// meaning that it only contains characters: a-z, A-Z, 0-9, _</para>
+        /// <para>Useful because most fonts do not support unusual characters</para>
+        /// </summary>
+        public bool IsDisplayNameFontSafe()
+        {
+            return ParseHelper.CheckNameRegex(tags.displayName);
+        }
+
+        /// <summary>
+        /// <para>Returns true if the chatter's message contains a given emote (by emote ID)</para>
+        /// <para>You can find emote IDs by using the Twitch API, or 3rd party sites</para>
+        /// </summary>
+        public bool ContainsEmote(string emoteId) => tags.ContainsEmote(emoteId);
+
+        /// <summary>
+        /// Returns true if the chatter has a given badge.
+        /// </summary>
+        public bool HasBadge(string badgeName) => tags.HasBadge(badgeName);
+    }
+}
---
+++ Assets/__Scripts/Twitch/TwitchManager.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Net.Sockets;
+using System.Threading;
+using UnityEngine;
+using Lexone.UnityTwitchChat;
+
+namespace EmoteGuesser.Twitch
+{
+    public class TwitchManager : MonoBehaviour
+    {
+    }
+}
